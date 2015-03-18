6bd3cb043c9709e9b9562d99fec86ecdbe720675
Eric Ting <eric.ting@stone-road.net>
2015-03-18 20:46:49 +0800

    fetch graph api and get facebook id

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,7 +2,29 @@
 
 angular.module('facebookFeed')
   .controller('MainCtrl', function($scope, $http) {
-    $scope.getGraph = function() {
+    $scope.getGraph = function(facebook_url) {
+      $http.get(encodeURI('https://graph.facebook.com/' + facebook_url))
+        .success(successCallback)
+        .error(errorCallback);
+    }
+
+    function successCallback(data, status) {
+      $scope.rep = data;
+      $scope.facebook_id = data['id'];
+      $scope.error = data['error'];
+    }
+
+    // {
+    //    "error": {
+    //       "message": "Unsupported get request. Please read the Graph API documentation at https://developers.facebook.com/docs/graph-api",
+    //       "type": "GraphMethodException",
+    //       "code": 100
+    //    }
+    // }
+
+
+
+    function errorCallback(data, status) {
 
     }
 
@@ -14,8 +36,8 @@ angular.module('facebookFeed')
       client.on("aftercopy", function(event) {
         // `this` === `client`
         // `event.target` === the element that was clicked
-        event.target.style.display = "none";
-        alert("Copied text to clipboard: " + event.data["text/plain"]);
+        // event.target.style.display = "none";
+        // alert("Copied text to clipboard: " + event.data["text/plain"]);
       });
     });
 
